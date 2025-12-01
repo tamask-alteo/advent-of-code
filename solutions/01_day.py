fbe4f0604ecb76b04ca4f3219e5440a65ddc47aa
Tamas Koncz <koncz.tamas@alteo.hu>
2025-12-01 21:40:54 +0100

    ✨ add D1/2 solution

--- solutions/01_day.py
+++ solutions/01_day.py
@@ -1,39 +1,60 @@
 START = 50
-MAX = 99
+CAP = 100
 
-def rotate(start: int, instruction: str) -> int:
+def parse_clicks(instruction: str) -> int:
     direction = instruction[0]
     clicks = int(instruction[1:])
     if direction == "L":
-        position = start - clicks
-        if position < 0:
-            position = MAX + 1 + position
+        clicks = -1 * clicks
+
+    return clicks
+
+def parse_rotations_and_remaining_clicks(clicks: int) -> tuple[int, int]:
+    if clicks == 0:
+        return (0, 0)
+    
+    if clicks > 0:
+        rotations = clicks // CAP
+        clicks = clicks % CAP
     else:
-        position = start + clicks
-        
-    return position % (MAX + 1)
+        clicks = abs(clicks)
+        rotations = clicks // CAP
+        clicks = -1 * (clicks % CAP)
+
+    return (rotations, clicks)
 
 def solve_day_1():
-    print(f"{START=}")
-    print(f"{MAX=}")
-
     with open("inputs/01_input.txt", "r") as f:
-        input = [line.strip() for line in f.readlines()]
+        instructions = [parse_clicks(line.strip()) for line in f.readlines()]
 
-    print(f"{input=}")
+    print(f"{instructions=}")
 
     zero_counter = 0
-    direction = START
-    for i in input:
-        direction = rotate(direction, i)
-        print(f"{i=}, {direction=}")
+    zero_counter_2 = 0
+    position = START
+    for clicks in instructions:
+        rotations, clicks = parse_rotations_and_remaining_clicks(clicks)
+        
+        previous_position = position
 
-        if direction == 0:
+        position = position + clicks
+
+        if position < 0 and previous_position != 0:
+            rotations += 1
+        if position > 100:
+            rotations += 1
+
+        position = position % CAP
+        
+        if position == 0:
             zero_counter += 1
+            rotations += 1
+
+        zero_counter_2 += rotations
 
     print(f"{zero_counter=}")
+    print(f"{zero_counter_2=}")
     
-
 
 if __name__ == "__main__":
     solve_day_1()
