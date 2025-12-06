9f48197d2409016378079d368bf375da82e83443
Tamas Koncz <koncz.tamas@alteo.hu>
2025-12-06 12:56:25 +0100

    ✨ add D4/2 solution

--- solutions/04_day.py
+++ solutions/04_day.py
@@ -24,8 +24,12 @@ class Grid:
             
         return self.data[r][c]
     
-    def get_all_coordinates(self):
+    def get_all_coordinates(self) -> List[List[int]]:
         return [[r, c] for c in range(self.ncols) for r in range(self.nrows)]
+    
+    def remove_roll(self, rc: tuple[int, int]) -> None:
+        r, c = rc
+        self.data[r][c] = False
 
 
 def solve_day_4():
@@ -33,9 +37,10 @@ def solve_day_4():
         grid = Grid([[x == "@" for x in line.strip()] for line in f.readlines()])
     
     neighbours = [[i, j] for j in [-1, 0, 1] for i in [-1, 0, 1] if not (i == 0 and j == 0)]
+    grid_coordinates = grid.get_all_coordinates()
 
     cnt = 0
-    for r, c in grid.get_all_coordinates():
+    for r, c in grid_coordinates:
         if not grid[r, c]: # not a roll
             continue
 
@@ -50,6 +55,32 @@ def solve_day_4():
             cnt += 1
 
     print(f"{cnt=}")
+
+    cnt_2 = 0
+    made_removal = True
+
+    while made_removal:
+        made_removal = False
+        
+        for r, c in grid_coordinates:
+            if not grid[r, c]: # not a roll
+                continue
+
+            cnt_adjacent_trues = 0
+            for i, j in neighbours:
+                    _r = r + i
+                    _c = c + j
+                    
+                    cnt_adjacent_trues += 1 if grid[_r, _c] else 0
+
+            if cnt_adjacent_trues < 4:
+                cnt_2 += 1
+                grid.remove_roll((r, c))
+                made_removal = True
+                break
+
+    print(f"{cnt_2=}")
+
 
 if __name__ == "__main__":
     solve_day_4()
